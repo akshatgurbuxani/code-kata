40f4b3dbca810d38b3368a5144eedb4bac24d051
Akshat Gurbuxani <imaksht@gmail.com>
2025-08-03 19:20:17 -0700

    feat: animal speak code kata oop implementation

---
+++ animal-speak/src/oop.py
@@ -0,0 +1,60 @@
+# system imports
+from abc import ABC, abstractmethod
+import logging
+
+# internal imports
+
+
+# external imports
+
+
+# logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class Animal(ABC):
+    def __init__(self, name: str, age: int):
+        """
+        Initializes the Animal object.
+        """
+        self.name = name
+        self.age = age
+        self.year = "year" if age == 1 else "years"
+
+    @abstractmethod
+    def speak(self) -> str:
+        """
+        Returns a string with the animal's name and age.
+        """
+        pass
+
+class Dog(Animal):
+    """
+    A Dog is an Animal.
+    """
+    def speak(self) -> str:
+        """
+        Returns a string with the animal's name and age.
+        """
+        return f"Ruff. My name is {self.name}, and I am {self.age} {self.year} old."
+    
+class Cat(Animal):
+    """
+    A Cat is an Animal.
+    """
+    def speak(self) -> str:
+        """
+        Returns a string with the animal's name and age.
+        """
+        return f"Meow. My name is {self.name}, and I am {self.age} {self.year} old."
+
+kitty = Cat("Kitty", 0)
+rex = Dog("Rex", 1)
+sylvester = Cat("Sylvester", 2)
+fido = Dog("Fido", 6)
+
+
+animals = (kitty, rex, sylvester, fido)
+
+for animal in animals:
+    print(animal.speak())
